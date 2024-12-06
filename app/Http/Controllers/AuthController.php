b2cf6fe9736b8c3eb5714d4a63bce39a9d9e9ae8
Md. Tarikul Islam <tarikulwebx@gmail.com>
2024-12-06 23:03:49 +0600

    Base with Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    /**
+     * Login a user by validating credentials
+     */
+    public function login(LoginRequest $request)
+    {
+        $inputs = $request->validated();
+
+        $user = User::where('email', $inputs['email'])->first();
+        if (!$user || !Hash::check($inputs['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'auth_token' => $token,
+            'user'       => new UserResource($user)
+        ], 200);
+    }
+
+
+    /**
+     * Register a new user
+     */
+    public function register(RegisterRequest $request)
+    {
+        $inputs = $request->validated();
+
+        $user = User::create($inputs);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'auth_token' => $token,
+            'user'       => new UserResource($user)
+        ], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// auth routes
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return new UserResource($request->user());
+    });
+});
